9425570e14aa7a5f2d0a28135a49f0c891a4e9c1
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-06-15 20:17:35 +0200

    Updated validation step, and internal pipeline

--- ramachandran.py
+++ ramachandran.py
@@ -13,16 +13,17 @@
 from pymol import cmd  # type: ignore
 import matplotlib.pyplot as plt
 import numpy
+from typing import Union, NoReturn, Optional
 
-supported_amino_acids = (
-    "arg", "his", "lys", "asp", "glu", "ser",
-    "thr", "asn", "gln", "cys", "sec", "gly",
-    "pro", "ala", "val", "ile", "leu", "met",
-    "phe", "tyr", "trp"
-)
+supported_amino_acids = {
+    "r": "arg", "h": "his", "k": "lys", "d": "asp", "e": "glu", "s": "ser",
+    "t": "thr", "n": "asn", "q": "gln", "c": "cys", "u": "sec", "g": "gly",
+    "p": "pro", "a": "ala", "v": "val", "i": "ile", "l": "leu", "m": "met",
+    "f": "phe", "y": "tyr", "w": "trp"
+}
 
 
-def ramachandran(model: tuple = None, resn: tuple = None):
+def ramachandran(model: Optional[tuple] = None, resn: Optional[Union[str, tuple]] = None) -> NoReturn:
     """A simple function to generate a Ramachandran plot from a pymol instance. If model and/or resn is provided as an argument it limits the selection within the instance.
 
     Parameters
@@ -31,83 +32,100 @@ def ramachandran(model: tuple = None, resn: tuple = None):
         Model/enzyme within the pymol instance, by default None
     resn : tuple, optional
         Resn/residue within the model to limit phi-psi points, by default None
-
-    Raises
-    ------
-    TypeError
-        Trigger if model input isn't a string, list or tuple.
-    TypeError
-        Trigger if any model in a list or tuple isn't a string.
-    ValueError
-        Trigger if model isn't in the current pymol instance.
-    ValueError
-        Trigger if model isn't in the current pymol instance.
-    TypeError
-        Trigger if resn isn't a string, list or tuple.
-    ValueError
-        Trigger if resn isn't supported by pymol.
-    TypeError
-        Trigger if resn in a list or tuple isn't a string.
-    ValueError
-        Trigger if resn isn't supported by pymol.
     """
-
-    # Validate that 'model' input is supported.
-    if not isinstance(model, (str, list, tuple)) and model is not None:
-        raise TypeError("'model' must be type 'str', 'list' or 'tuple'.")
-
-    if isinstance(model, (list, tuple)):
-        for element in model:
-            if not isinstance(model, str):
-                raise TypeError("'model' can only contain type 'str'.")
 
     local_model = cmd.get_names(type="public_objects")
 
-    if isinstance(model, str) and not model in local_model:
-        raise ValueError(f"Model '{model}' isn't in the pymol instance.")
+    # -- Model input validation
+    if model is not None:
+        if isinstance(model, (str, list, tuple)):
+            if isinstance(model, str):
+                temp = []
+                temp.append(model)
+                model = temp
+                del temp
 
-    if isinstance(model, (list, tuple)) and not model in local_model:
-        for element in model:
-            if not element in local_model:
-                raise ValueError(
-                    f"Model '{element}' isn't in the pymol instance.")
+            if isinstance(model, tuple):
+                model = list(model)
 
-        # Validate that 'resn' input is supported.
-    if not isinstance(resn, (str, list, tuple)) and resn is not None:
-        raise TypeError("'resn' must be type 'str', 'list' or 'tuple'.")
+            for value in model:
+                if not isinstance(value, str):
+                    raise TypeError("'model' can only contain type 'str'.")
 
-    if isinstance(resn, str) and not resn in supported_amino_acids:
-        raise ValueError(f"'{resn}' isn't a supported amino acid residue.")
+                if value not in local_model:
+                    cmd.fetch(value)
+        else:
+            raise TypeError("'model' must be type 'str', 'list' or 'tuple'.")
 
-    if isinstance(resn, (list, tuple)):
-        for residue in resn:
-            if not isinstance(residue, str):
-                raise TypeError("'resn' can only contain type 'str'.")
+    # -- Residue input validation
+    if resn is not None:
+        # Residue input is either string, list or tuple, else throw a TypeError
+        if isinstance(resn, (str, list, tuple)):
+            # Convert string and tuple to list type
+            if isinstance(resn, tuple):
+                resn = list(resn)
 
-            if not residue in supported_amino_acids:
-                raise ValueError(
-                    f"'{residue}' isn't a supported amino acid residue.")
+            if isinstance(resn, str):
+                temp = []
+                temp.append(resn)
+                resn = temp
+                del temp
 
+            # Ensure valid residue codes
+            for key, value in enumerate(resn):
+                value = value.lower()
+
+                if not isinstance(value, str):
+                    raise TypeError("'resn' can only contain type 'str'.")
+
+                if len(value) == 3:
+                    if value not in supported_amino_acids.values():
+                        ValueError(
+                            f"'{value}' isn't a supported amino acid residue.")
+                elif len(value) == 1:
+                    if value not in supported_amino_acids.keys():
+                        ValueError(
+                            f"'{value}' isn't a supported amino acid residue.")
+
+                    resn[key] = supported_amino_acids[value]
+                else:
+                    ValueError(
+                        f"'{value}' isn't a supported amino acid residue.")
+
+        else:
+            raise TypeError("'resn' must be type 'str', 'list' or 'tuple'.")
+
+    # If model is None
     if not model:
+        if not local_model:
+            raise RuntimeError("Neither a choosen, nor a local model to use.")
+
         model = local_model
         del local_model
+
+    # If residue is non, include all amino acid values
+    if resn is None:
+        resn = list(supported_amino_acids.values())
+
+    sString = []
+    sCount = []
+    phi_psi = []
 
     # Cycle through models
     for element in model:
         # Select model and/or from resn depending on condition.
-        if resn is not None:
-            cmd.select(f"sele_{element}", f"m. {element} & r. {resn}")
-        else:
-            cmd.select(f"sele_{element}", f"m. {element}")
+        for key, value in enumerate(resn):
+            sString.append(f"sele_{element}_{value}")
+            sCount.append(cmd.select(
+                sString[key], f"m. {element} & r. {value}"))
 
         # Get all phi/psi values from entire structure
-        phi_psi = cmd.phi_psi(selection=f"sele_{element}")
+        for value in sString:
+            phi_psi.append(cmd.phi_psi(selection=value))
 
         # Remove intermediate selection
-        cmd.delete(f"sele_{element}")
-
-        # Seperate phi_psi into phi and psi.
-        phi, psi = zip(*phi_psi.values())
+        for value in sString:
+            cmd.delete(value)
 
         plt.figure(figsize=(8.5, 5.0), dpi=100)
         plt.title(f"Ramachandran plot (PDBid: {element.upper()})")
@@ -146,9 +164,18 @@ def ramachandran(model: tuple = None, resn: tuple = None):
                     levels=[10**i for i in range(-7, 0)],
                     antialiased=True, extent=(min_bound, max_bound) * 2, alpha=0.55)
 
-        if resn is not None:
-            plt.scatter(phi, psi, marker=".", s=3, c="k")
-        else:
+        # Seperate phi_psi into phi and psi.
+        for key, value in enumerate(phi_psi):
+            try:
+                phi, psi = zip(*value.values())
+            except ValueError as E:
+                if not sCount[key] == 0:
+                    raise E
+                else:
+                    phi, psi = (None, None)
+
+            # TODO: Variable for color + label customization
+
             plt.scatter(phi, psi, marker=".", s=3, c="k")
 
         plt.show()
